8acbe68efa27473712a1e52c9753de50ab3e3373
Ryan McCloy <ryanmccloy@Ryans-MacBook-Air.local>
2024-12-07 13:03:36 +0200

    fixed A11y issue

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -2,13 +2,19 @@ import AboutUsContent from "../components/AboutUsContent";
 
 function AboutUs() {
   return (
-    <div className="px-30 pt-90 lg:pt-120 pb-30 flex flex-col gap-60">
-      <h2 className="text-end text-3xl uppercase md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+    <section
+      className="px-30 pt-90 lg:pt-120 pb-30 flex flex-col gap-60"
+      aria-labelledby="about-us-heading"
+    >
+      <h2
+        className="text-end text-3xl uppercase md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl"
+        id="about-us-heading"
+      >
         Websites Built <br></br> For{" "}
         <span className="text-accent">Success</span>
       </h2>
       <AboutUsContent />
-    </div>
+    </section>
   );
 }
 
