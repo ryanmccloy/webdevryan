6cb5e7a267bc1aa354ada9c74e2474651d3d3841
Ryan McCloy <ryanmccloy@Ryans-MacBook-Air.local>
2024-12-10 20:52:38 +0000

    updated footer margin

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="mt-120 bg-primary p-30 text-background lg:p-60">
+    <footer className="mt-60 bg-primary p-30 text-background md:mt-90 lg:p-60">
       <div className="flex flex-col gap-30 sm:flex-row sm:justify-between">
         <span>{"<I'M-RYAN/>"}</span>
         <span>contact@ryanmccloywebdevelopment.com</span>
